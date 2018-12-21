5365654ba6da2384c42f8a042d3608b3dc13e7bd
Eva Stolz <evasmail@gmail.com>
2018-12-22 00:12:24 +0100

    4.5.x Add loop to dynamically change # of carousel indicators

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,6 +1,7 @@
 class SimplePagesController < ApplicationController
   def index
-  	@featured_products = Product.order(:updated_at).last(3)
+    @featured_products = Product.order(:updated_at).last(3)
+    @number_of_featured_products = @featured_products.length
   end
 
   def about
