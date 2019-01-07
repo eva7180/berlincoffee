eb0ca1e9124d5025020940a20babad210974ed55
Eva Stolz <evasmail@gmail.com>
2019-01-07 12:15:10 +0100

    Add contact form and implement SendGrid in production

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -10,4 +10,16 @@ class SimplePagesController < ApplicationController
   def contact
   end
 
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+
+  	ActionMailer::Base.mail(
+  		from: @email, 
+  		to: 'evasmail@gmail.com',
+  		subject: "A new contact form message from #{@name}",
+  		body: @message).deliver_now
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-  get 'simple_pages/index'
-  get 'simple_pages/about'
-  get 'simple_pages/contact'
   
+  get 'simple_pages/about', to: "simple_pages#about"
+  get 'simple_pages/contact', to: "simple_pages#contact"
+  post 'simple_pages/thank_you'
+
   root 'simple_pages#index'
 end
