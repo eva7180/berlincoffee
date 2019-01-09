c880338a43a7435f0e260fe83210a2676fce63c6
Eva Stolz <evasmail@gmail.com>
2019-01-09 14:31:14 +0100

    Merge Reviews into Master (#4)
    * 5.5 Add CanCanCan for page authorizations
        Allow additional parameters on sign_up and account_update
        Clean up and re-link user and devise views, routes and controller
        Add current logged in  user to navbar
    
    * Bugfix in navbar
    
    * Bugfix in users#show view
    
    * Add Comment Model and style Product index and show pages
    
    * 5.8 Create Comments controller, nested routes and view partials
    
    * 5.8 Style comments input form and fix syntax error in comments controller
    
    * 5.8 Add Raty Plugin for star rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments, dependent: :destroy
 
 # helper method to display prices with 2 decimals
   def display_price
@@ -10,6 +11,18 @@ class Product < ApplicationRecord
 
   def self.search(search_term)
     Product.where("name ilike ?", "%#{search_term}%")
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_desc.last
+  end
+
+  def average_rating
+    comments.average(:rating).to_f
   end
 
 end
