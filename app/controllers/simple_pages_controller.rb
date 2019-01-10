fcdb7e17705c396d3314c84f606555561a9dee14
Eva Stolz <evasmail@gmail.com>
2019-01-10 12:20:33 +0100

    5.7 Use highest_rating_comment method on landing page and fix image sizing for product images

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,6 +1,13 @@
 class SimplePagesController < ApplicationController
   def index
-    @featured_products = Product.order(:updated_at).last(3)
+    @products = Product.all
+    @reviewed_products = []
+    @products.each do |product|
+      if product.comments.count > 0
+        @reviewed_products << product
+      end
+    end
+    @featured_products = @reviewed_products.last(3)
     @number_of_featured_products = @featured_products.length
   end
 
