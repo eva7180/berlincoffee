9a4aa1ec9b0c94320be94fd49af748d143c977d6
Eva Stolz <evasmail@gmail.com>
2019-01-10 19:03:35 +0100

    5.10 Add Admin flag, functionality and restrictions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,14 @@ class Ability
 
   def initialize(user)
     user ||= User.new #guest user (not logged in)
-    can :manage, User, id: user.id
+    can :manage, User, id: user.id # permissions for every user, even if not logged in  
+    if user.present?  # additional permissions for logged in users
+      can :create, Comment
+      if user.admin?  # additional permissions for administrators
+        can :destroy, Comment
+        can :manage, User
+      end
+    end
   end
+  
 end
