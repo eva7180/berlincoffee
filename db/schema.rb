a7633eaeef063abe45115afccf7069a9f3a355e7
Eva Stolz <evasmail@gmail.com>
2019-01-20 15:46:23 +0100

    Style order view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_20_135237) do
+ActiveRecord::Schema.define(version: 2019_01_20_143957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2019_01_20_135237) do
     t.integer "user_id"
     t.string "status"
     t.decimal "total_price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
