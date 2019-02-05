9234e80d619e1074073736d38b8f4ea1b1370133
Eva Stolz <evasmail@gmail.com>
2019-02-05 15:38:44 +0100

    Display data to all users

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -10,6 +10,9 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
     $(".alert.alert-info").show();
+    $('#product-comments').prepend(data.comment);
+    $("#average-rating").attr('data-score', data.average_rating);
+    updateRating();
   },
 
   listen_to_comments: function() {
