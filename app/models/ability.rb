540e1123ea9f9f6cd9606b930fd189cfe827029e
Eva Stolz <evasmail@gmail.com>
2019-02-06 17:34:50 +0100

    Fix Order#index to show own orders only and restrict access

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,7 @@ class Ability
     can :read, Product # permissions for every user, even if not logged in
     if user.present?  # additional permissions for logged in users
       can [:create, :destroy], Comment, user_id: user.id
-      can :manage, User, id: user.id
+      can [:show, :create, :update, :destroy], User, id: user.id #can manage own user, but can NOT index all users
       can :manage, Order, user_id: user.id
       if user.admin?  # additional permissions for administrators
         can :manage, :all
