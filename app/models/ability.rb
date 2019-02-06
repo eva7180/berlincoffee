913d367882b34791c90872779114944b91856fac
Eva Stolz <evasmail@gmail.com>
2019-02-06 16:33:22 +0100

    Restrict permissions for Product to Admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,13 +3,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new #guest user (not logged in)
-    can :manage, User, id: user.id # permissions for every user, even if not logged in
+    can :read, [Product, Comment] # permissions for every user, even if not logged in
     if user.present?  # additional permissions for logged in users
-      can :create, Comment
+      can :manage, Comment, user_id: user.id
+      can :manage, User, id: user.id
       can :manage, Order, user_id: user.id
       if user.admin?  # additional permissions for administrators
-        can :destroy, Comment
-        can :manage, User
+        can :manage, :all
       end
     end
   end
