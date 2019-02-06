013bb9424210d0e4aa23e5add04fc91b49aa6f07
Eva Stolz <evasmail@gmail.com>
2019-02-06 16:51:33 +0100

    Adjust user permissions for own Comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,9 @@ class Ability
 
   def initialize(user)
     user ||= User.new #guest user (not logged in)
-    can :read, [Product, Comment] # permissions for every user, even if not logged in
+    can :read, Product # permissions for every user, even if not logged in
     if user.present?  # additional permissions for logged in users
-      can :manage, Comment, user_id: user.id
+      can [:create, :destroy], Comment, user_id: user.id
       can :manage, User, id: user.id
       can :manage, Order, user_id: user.id
       if user.admin?  # additional permissions for administrators
