2ced7c2dafcfc9f2e3d545f2b5d1feaf7987dea7
Eva Stolz <evasmail@gmail.com>
2019-02-06 19:21:05 +0100

    Fix products_controller_tests after adding user restrictions for Product

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
   let!(:test_product) { FactoryBot.create(:product) }
+  let(:admin) { FactoryBot.create(:admin_user) }
+  let(:user) { FactoryBot.create(:user) }
 
   describe 'GET #index' do
     it 'renders the products/index template' do
@@ -30,109 +32,239 @@ RSpec.describe ProductsController, type: :controller do
   end
 
   describe 'GET #new' do
-    it 'renders the products/new template' do
-      get :new
-      expect(response).to be_ok
-      expect(response).to render_template('new')
+    context 'when user is logged in and has an admin role' do
+      before do
+        sign_in admin
+      end
+
+      it 'renders the products/new template' do
+        get :new
+        expect(response).to be_ok
+        expect(response).to render_template('new')
+      end
+    end
+
+    context 'when user is logged in and does not have admin role' do
+      before do
+        sign_in user
+      end
+      
+      it 'redirects to root' do
+        get :new
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'when user is not logged in' do
+      it 'redirects to root' do
+        get :new
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 
   describe 'GET #edit' do
-    it 'loads correct product' do
-      get :edit, params: { id: test_product.id }
-      expect(assigns(:product)).to eq test_product
+    context 'when user is logged in and has an admin role' do
+      before do
+        sign_in admin
+      end
+
+      it 'loads correct product' do
+        get :edit, params: { id: test_product.id }
+        expect(assigns(:product)).to eq test_product
+      end
+      
+      it 'renders the products/edit template' do
+        get :edit, params: { id: test_product.id }
+        expect(response).to be_ok
+        expect(response).to render_template('edit')
+      end
     end
-    
-    it 'renders the products/edit template' do
-      get :edit, params: { id: test_product.id }
-      expect(response).to be_ok
-      expect(response).to render_template('edit')
+
+    context 'when user is logged in and does not have admin role' do
+      before do
+        sign_in user
+      end
+      
+      it 'redirects to root' do
+        get :new
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'when user is not logged in' do
+      it 'redirects to root' do
+        get :new
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 
   describe 'POST products#create' do
-    context 'with valid attributes' do
-      it 'should save the new product in the database' do
-        expect {
-          post :create, params: { product: { name:  "Any Name" } }
-        }.to change(Product, :count).by(1)
+    context 'when user is logged in and has an admin role' do
+      before do
+        sign_in admin
       end
 
-      it 'should redirect to the product#show page' do
-        post :create, params: { product: { name:  "Any Name" } }
-        expect(response).to redirect_to Product.last
+      context 'with valid attributes' do
+        it 'should save the new product in the database' do
+          expect {
+            post :create, params: { product: { name:  "Any Name" } }
+          }.to change(Product, :count).by(1)
+        end
+
+        it 'should redirect to the product#show page' do
+          post :create, params: { product: { name:  "Any Name" } }
+          expect(response).to redirect_to Product.last
+        end
+      end
+
+      context 'with invalid attributes' do
+        it 'should not save the new product in the database' do
+          expect {
+            post :create, params: { product: { description:  "Any text" } }
+          }.to_not change(Product, :count)
+        end
+
+        it 'should render product#new template' do
+          post :create, params: { product: { description:  "Any text" } }
+          expect(response).to render_template('new')
+        end
       end
     end
 
-    context 'with invalid attributes' do
-      it 'should not save the new product in the database' do
-        expect {
-          post :create, params: { product: { description:  "Any text" } }
-        }.to_not change(Product, :count)
+    context 'when user is logged in and does not have admin role' do
+      before do
+        sign_in user
       end
+      
+      it 'redirects to root' do
+        get :new
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
 
-      it 'should render product#new template' do
-        post :create, params: { product: { description:  "Any text" } }
-        expect(response).to render_template('new')
+    context 'when user is not logged in' do
+      it 'redirects to root' do
+        get :new
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
       end
     end
   end
 
   describe 'PATCH/PUT products#update' do
-    context 'with valid attributes' do
-      it 'loads correct product' do
-        put :update, params: { id: test_product.id, product: { name: "Edited Name" } }
-        expect(assigns(:product)).to eq Product.last
+    context 'when user is logged in and has an admin role' do
+      before do
+        sign_in admin
       end
 
-      it "changes product's attributes" do
-        put :update, params: { id: test_product.id, product: { name: "Edited Name", description: "Edited", variety: "Edited" } }
-        test_product.reload
-        expect(test_product.description).to eq("Edited")
-        expect(test_product.variety).to eq("Edited")
+      context 'with valid attributes' do
+        it 'loads correct product' do
+          put :update, params: { id: test_product.id, product: { name: "Edited Name" } }
+          expect(assigns(:product)).to eq Product.last
+        end
+
+        it "changes product's attributes" do
+          put :update, params: { id: test_product.id, product: { name: "Edited Name", description: "Edited", variety: "Edited" } }
+          test_product.reload
+          expect(test_product.description).to eq("Edited")
+          expect(test_product.variety).to eq("Edited")
+        end
+
+        it 'redirects to products index page' do
+          put :update, params: { id: test_product.id, product: { name: "Edited Name" } }
+          expect(response).to redirect_to products_path
+        end
       end
 
-      it 'redirects to products index page' do
-        put :update, params: { id: test_product.id, product: { name: "Edited Name" } }
-        expect(response).to redirect_to products_path
+      context 'with invalid attributes' do
+        it 'loads correct product' do
+          put :update, params: { id: test_product.id, product: { name: nil } }
+          expect(assigns(:product)).to eq Product.last
+        end
+
+        it "does not change product's attributes" do
+          put :update, params: { id: test_product.id, product: { name: nil, description: "Edited", variety: "Edited" } }
+          test_product.reload
+          expect(test_product.description).to_not eq("Edited")
+          expect(test_product.variety).to_not eq("Edited")
+        end
+
+        it 're-renders the products edit template' do
+          put :update, params: { id: test_product.id, product: { name: nil } }
+          expect(response).to render_template('edit')
+        end
       end
     end
 
-    context 'with invalid attributes' do
-      it 'loads correct product' do
-        put :update, params: { id: test_product.id, product: { name: nil } }
-        expect(assigns(:product)).to eq Product.last
+    context 'when user is logged in and does not have admin role' do
+      before do
+        sign_in user
       end
-
-      it "does not change product's attributes" do
-        put :update, params: { id: test_product.id, product: { name: nil, description: "Edited", variety: "Edited" } }
-        test_product.reload
-        expect(test_product.description).to_not eq("Edited")
-        expect(test_product.variety).to_not eq("Edited")
+      
+      it 'redirects to root' do
+        get :new
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
       end
+    end
 
-      it 're-renders the products edit template' do
-        put :update, params: { id: test_product.id, product: { name: nil } }
-        expect(response).to render_template('edit')
+    context 'when user is not logged in' do
+      it 'redirects to root' do
+        get :new
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
       end
     end
   end
 
   describe 'DELETE products#destroy' do
-    it 'loads correct product' do
-      delete :destroy, params: { id: test_product.id }
-      expect(assigns(:product)).to eq test_product
-    end
+    context 'when user is logged in and has an admin role' do
+      before do
+        sign_in admin
+      end
 
-    it 'deletes product from database' do
-      expect {
+      it 'loads correct product' do
         delete :destroy, params: { id: test_product.id }
-        }.to change(Product, :count).by(-1)
+        expect(assigns(:product)).to eq test_product
+      end
+
+      it 'deletes product from database' do
+        expect {
+          delete :destroy, params: { id: test_product.id }
+          }.to change(Product, :count).by(-1)
+      end
+
+      it 'redirects to products index page' do
+        delete :destroy, params: { id: test_product.id }
+        expect(response).to redirect_to products_path
+      end
     end
 
-    it 'redirects to products index page' do
-      delete :destroy, params: { id: test_product.id }
-      expect(response).to redirect_to products_path
+    context 'when user is logged in and does not have admin role' do
+      before do
+        sign_in user
+      end
+      
+      it 'redirects to root' do
+        get :new
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'when user is not logged in' do
+      it 'redirects to root' do
+        get :new
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 
